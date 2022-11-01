0f0279c2f96291d227668e1cc9467a512c963cb6
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-01 21:37:56 +0100

    Add route to active-projects component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActiveProjectsComponent } from './project-administration/active-projects/active-projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'active-projects', component: ActiveProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
