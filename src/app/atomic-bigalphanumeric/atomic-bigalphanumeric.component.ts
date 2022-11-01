f25b898176f1876d20663cf1f568c34740f1f547
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-01 20:56:58 +0100

    Fix property binding for atomic-bigalphanumeric component

--- src/app/atomic-bigalphanumeric/atomic-bigalphanumeric.component.ts
+++ src/app/atomic-bigalphanumeric/atomic-bigalphanumeric.component.ts
@@ -1,3 +1,4 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atomic-bigalphanumeric.component.css']
 })
 export class AtomicBIGALPHANUMERICComponent implements OnInit {
+  @Input() property!: string | Array<string>;
 
   constructor() { }
 
