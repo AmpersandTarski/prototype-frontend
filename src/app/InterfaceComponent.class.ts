a912c4fb1d02afa8c0a258e5a3861b00f9f5606a
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-02 12:53:30 +0100

    Atomic alphanumeric extends AtomicComponent class. Move share properties
    #37

--- src/app/InterfaceComponent.class.ts
+++ src/app/InterfaceComponent.class.ts
@@ -1,1 +1,8 @@
-export abstract class InterfaceComponent {}
+export abstract class InterfaceComponent {
+  public crudC: boolean = false;
+  public crudR: boolean = true;
+  public crudU: boolean = false;
+  public crudD: boolean = false;
+  public exprIsUni: boolean = false;
+  public exprIsTot: boolean = false;
+}
--- src/app/atomic-alphanumeric/atomic-alphanumeric.component.ts
+++ src/app/atomic-alphanumeric/atomic-alphanumeric.component.ts
@@ -1,31 +1,21 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { AtomicComponent } from '../AtomicComponent.class';
 
 @Component({
   selector: 'app-atomic-alphanumeric',
   templateUrl: './atomic-alphanumeric.component.html',
-  styleUrls: ['./atomic-alphanumeric.component.css']
+  styleUrls: ['./atomic-alphanumeric.component.css'],
 })
-export class AtomicALPHANUMERICComponent implements OnInit {
-  public crudC: boolean;
-  public crudR: boolean;
-  public crudU: boolean;
-  public crudD: boolean;
-  public exprIsUni: boolean;
-  public exprIsTot: boolean;
-
+export class AtomicALPHANUMERICComponent
+  extends AtomicComponent
+  implements OnInit
+{
   @Input() property!: string | Array<string>;
 
   constructor() {
-    this.crudC = false;
-    this.crudR = false;
-    this.crudU = false;
-    this.crudD = false;
-    this.exprIsUni = false;
-    this.exprIsTot = false;
+    super();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
