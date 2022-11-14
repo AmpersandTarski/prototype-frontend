00f02831352b4b07a0c58a6cb3d7ad5ece17f75e
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-14 17:46:36 +0100

    Add empty component for PersonInterface

--- src/app/project-administration/project-administration.module.ts
+++ src/app/project-administration/project-administration.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ActiveProjectsComponent } from './active-projects/active-projects.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProjectComponent } from './project/project.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
-  declarations: [ActiveProjectsComponent, ProjectComponent],
+  declarations: [ActiveProjectsComponent, ProjectComponent, PersonComponent],
   imports: [CommonModule, SharedModule],
 })
 export class ProjectAdministrationModule {}
