8368ce223f4a841697e23e3d061c2a1024652a28
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-19 12:17:00 +0100

    Add type support for Box header and row directives to have type hinting when using those templates

--- src/app/shared/box-components/box-table/box-table.component.ts
+++ src/app/shared/box-components/box-table/box-table.component.ts
@@ -1,14 +1,44 @@
 import { Component, ContentChild, Directive, Input, OnInit, TemplateRef } from '@angular/core';
 
+interface BoxTableHeaderTemplateContext<TItem extends object> {
+  $implicit: TItem[];
+}
+
 @Directive({
   selector: 'ng-template[boxTableHeader]',
 })
-export class BoxTableHeaderTemplateDirective {}
+export class BoxTableHeaderTemplateDirective<TItem extends object> {
+  @Input('boxTableHeader') data!: TItem[] | '';
+
+  // This context guard provides type hinting when using template directive
+  // To understand why/how: https://www.youtube.com/watch?v=dau7kQMdH4A
+  static ngTemplateContextGuard<TContextItem extends object>(
+    dir: BoxTableHeaderTemplateDirective<TContextItem>,
+    ctx: unknown,
+  ): ctx is BoxTableHeaderTemplateContext<TContextItem> {
+    return true;
+  }
+}
+
+interface BoxTableRowTemplateContext<TItem extends object> {
+  $implicit: TItem;
+}
 
 @Directive({
   selector: 'ng-template[boxTableRow]',
 })
-export class BoxTableRowTemplateDirective {}
+export class BoxTableRowTemplateDirective<TItem extends object> {
+  @Input('boxTableRow') data!: TItem[];
+
+  // This context guard provides type hinting when using template directive
+  // To understand why/how: https://www.youtube.com/watch?v=dau7kQMdH4A
+  static ngTemplateContextGuard<TContextItem extends object>(
+    dir: BoxTableRowTemplateDirective<TContextItem>,
+    ctx: unknown,
+  ): ctx is BoxTableRowTemplateContext<TContextItem> {
+    return true;
+  }
+}
 
 @Component({
   selector: 'app-box-table',
