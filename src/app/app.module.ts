774533eefa76d921c92e3a727bd24e40a02c6e07
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-20 11:47:19 +0100

    Add HttpClient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +15,7 @@ import { ToolsModule } from './tools/tools.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     AppLayoutModule,
     SharedModule,
