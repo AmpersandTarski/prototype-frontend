b608d2f026e0c0d18d0bf62b2d041550f53b9692
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-21 11:13:27 +0100

    Add content for HomeComponent

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -18,6 +18,8 @@ import { AppConfigModule } from './config/config.module';
 import { AppSidebarComponent } from "./app.sidebar.component";
 import { AppLayoutComponent } from "./app.layout.component";
 import { HomeComponent } from './home/home.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
     declarations: [
@@ -37,6 +39,8 @@ import { HomeComponent } from './home/home.component';
         InputTextModule,
         SidebarModule,
         BadgeModule,
+        CardModule,
+        ButtonModule,
         RadioButtonModule,
         InputSwitchModule,
         RippleModule,
