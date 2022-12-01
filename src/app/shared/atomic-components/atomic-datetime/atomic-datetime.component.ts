d9f7da209c01d03fd21a4503daca0985bd347224
Patrick Ramge <patrickramge@gmail.com>
2022-12-01 13:59:49 +0000

    add extra properties to datetime field

--- src/app/shared/atomic-components/atomic-datetime/atomic-datetime.component.ts
+++ src/app/shared/atomic-components/atomic-datetime/atomic-datetime.component.ts
@@ -7,5 +7,11 @@ import { BaseComponent } from '../../BaseComponent.class';
   styleUrls: ['./atomic-datetime.component.css'],
 })
 export class AtomicDatetimeComponent extends BaseComponent {
-  @Input() property!: any | Array<any>;
+  @Input() property: any | Array<any>;
+  public datumpie: Date = new Date('1995-12-17T03:24:00');
+  @Input() format: string = 'yy-mm-dd';
+
+  public changed() {
+    console.log(this.property);
+  }
 }
