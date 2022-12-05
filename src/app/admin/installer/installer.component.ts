3def4bba5629df340da2ffab83a2056fc842e3e6
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-12-05 16:48:50 +0100

    Fixes after comments in PR #80

--- src/app/admin/installer/installer.component.ts
+++ src/app/admin/installer/installer.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import { finalize } from 'rxjs';
 
 class ButtonState {
-  loading: boolean = false;
-  success: boolean = false;
-  error: boolean = false;
+  public loading: boolean = false;
+  public success: boolean = false;
+  public error: boolean = false;
 
   isLoading(): boolean {
     return this.loading;
@@ -28,20 +28,34 @@ class ButtonState {
   styleUrls: ['./installer.component.scss'],
 })
 export class InstallerComponent {
-  buttonState1 = new ButtonState();
-  buttonState2 = new ButtonState();
-  buttonState3 = new ButtonState();
+  buttonState1: ButtonState = new ButtonState();
+  buttonState2: ButtonState = new ButtonState();
+  buttonState3: ButtonState = new ButtonState();
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Method to determine if there is a 'installer' call to the backend in progress, triggered by any of the buttons
+   * We capture this state in the associated buttonState(s)
+   */
   isLoading(): boolean {
     return [this.buttonState1, this.buttonState2, this.buttonState3].some(state => state.isLoading());
   }
 
+  /**
+   * Set the buttonStates to their initial value
+   */
   initButtonStates(): void {
     [this.buttonState1, this.buttonState2, this.buttonState3].forEach(state => state.init());
   }
 
+  /**
+   * Call to the backend to (re)install the application and database
+   *
+   * @param defaultPop determines if default population of the application should be installed or not
+   * @param ignoreInvariants determines if invariant violations of the default population should be ignored or not
+   * @param buttonState the ButtonState associated with the button that triggers this method
+   */
   reinstall(defaultPop: boolean, ignoreInvariants: boolean, buttonState: ButtonState): void {
     this.initButtonStates();
     buttonState.loading = true;
