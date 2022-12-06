52388e8b1ce23f75063e535358b596ac0858be39
Patrick Ramge <patrickramge@gmail.com>
2022-12-06 14:48:32 +0100

    replace dummy data with backend data from observable

--- src/app/project-administration/people/people.component.ts
+++ src/app/project-administration/people/people.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { testdata } from './testdata';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from '../backend.service';
+import { PersonInterface } from '../person/person.interface';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './people.component.html',
   styleUrls: ['./people.component.scss'],
 })
-export class PeopleComponent {
-  data = testdata;
+export class PeopleComponent implements OnInit {
+  data$!: Observable<PersonInterface[]>;
 
-  constructor() {}
+  constructor(private service: BackendService) {}
+
+  ngOnInit(): void {
+    this.data$ = this.service.getPeople();
+  }
 }
