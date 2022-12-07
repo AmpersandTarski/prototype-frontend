a3ec99eb33b6cec8ba015588c6f75f762b9b0d8f
Patrick Ramge <patrickramge@gmail.com>
2022-12-07 14:24:11 +0100

    remove toArray method & generate menu items in ts file

--- src/app/shared/atomic-components/atomic-object/atomic-object.component.ts
+++ src/app/shared/atomic-components/atomic-object/atomic-object.component.ts
@@ -3,6 +3,7 @@ import { MenuItem } from 'primeng/api';
 import { BaseComponent } from '../../BaseComponent.class';
 import { Router } from '@angular/router';
 import { InterfaceRefObject, ObjectBase } from '../../objectBase.interface';
+type AtomicObjectMenuItem = any;
 
 @Component({
   selector: 'app-atomic-object',
@@ -10,24 +11,30 @@ import { InterfaceRefObject, ObjectBase } from '../../objectBase.interface';
   styleUrls: ['./atomic-object.component.scss'],
 })
 export class AtomicObjectComponent extends BaseComponent implements OnInit {
-  @Input() property!: ObjectBase | Array<ObjectBase>;
+  private _objects!: Array<ObjectBase>;
+  public menuItems: AtomicObjectMenuItem = [];
+
+  @Input()
+  set property(value: ObjectBase | Array<ObjectBase>) {
+    this._objects = this.requireArray(value);
+  }
+  get property(): Array<ObjectBase> {
+    return this._objects;
+  }
 
   constructor(private router: Router) {
     super();
   }
 
-  public navigateToEntity(type: string, id: string) {
-    this.router.navigate(['p', type.toLowerCase(), `${id}`]);
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this._objects.forEach((object) => {
+      this.menuItems[object._id_] = this.toPrimeNgMenuModel(object._ifcs_, object._id_);
+    });
   }
 
-  public toArray(prop: ObjectBase | Array<ObjectBase>): Array<ObjectBase> {
-    if (Array.isArray(prop)) {
-      return prop;
-    } else if (prop === null) {
-      return [];
-    } else {
-      return [prop];
-    }
+  public navigateToEntity(type: string, id: string) {
+    this.router.navigate(['p', type.toLowerCase(), `${id}`]);
   }
 
   public toPrimeNgMenuModel(ifcs: Array<InterfaceRefObject>, id: string): Array<MenuItem> {
