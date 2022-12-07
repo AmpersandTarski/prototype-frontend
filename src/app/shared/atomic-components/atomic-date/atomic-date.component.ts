60d389779842ddd03c39fb645ecb94dcb7bfa309
LiWeiYeh <liwei_yeh@hotmail.com>
2022-12-07 15:00:38 +0100

    fix: atomic date permissions

--- src/app/shared/atomic-components/atomic-date/atomic-date.component.ts
+++ src/app/shared/atomic-components/atomic-date/atomic-date.component.ts
@@ -7,7 +7,7 @@ import { BaseComponent } from '../../BaseComponent.class';
   styleUrls: ['./atomic-date.component.css'],
 })
 export class AtomicDateComponent extends BaseComponent {
-  @Input() property!: string | null;
+  @Input() property: string | null = null;
   // Possible formats can be found at https://www.primefaces.org/primeng/calendar.
   // Scroll down to DateFormat for the documentation
   @Input() format: string = 'yy-mm-dd';
