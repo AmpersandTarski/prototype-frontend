749b854a97add1e4a2a090f00976a78b77ff1824
Patrick Ramge <patrickramge@gmail.com>
2022-12-20 09:16:09 +0100

    replace projects interface with seperate interfaces for active and inactive projects

--- src/app/project-administration/active-projects/active-projects.interface.ts
+++ src/app/project-administration/active-projects/active-projects.interface.ts
@@ -1,6 +1,5 @@
-import { ObjectBase, PersonNameView, ProjectNameView, ProjectsInterface } from "../projects.interface";
-
-export interface ActiveProjectsInterface extends Omit<ProjectsInterface, 'Name'> {
+export interface ActiveProjectsInterface extends ObjectBase {
+  _view_: ProjectNameView;
   _sortValues_: {
     Projects: string;
     Name: string;
@@ -15,8 +14,37 @@ export interface ActiveProjectsInterface extends Omit<ProjectsInterface, 'Name'>
     _view_: ProjectNameView;
   };
   Name: string;
+  Description: string;
+  Projectleider: ObjectBase & {
+    _view_: PersonNameView;
+  };
   _64_: ObjectBase & {
     _view_: PersonNameView;
   };
   Start: string | null;
+  Status: string;
+  Active: boolean;
+  _id_: string;
+}
+
+interface PersonNameView {
+  firstName: string;
+  txt1: string;
+  lastName: string;
+}
+
+interface ProjectNameView {
+  name: string;
+}
+
+interface ObjectBase {
+  _id_: string;
+  _label_: string;
+  _path_: string;
+  _ifcs_: Array<InterfaceRefObject>;
+}
+
+interface InterfaceRefObject {
+  id: string;
+  label: string;
 }
--- src/app/project-administration/inactive-projects/inactive-projects.interface.ts
+++ src/app/project-administration/inactive-projects/inactive-projects.interface.ts
@@ -1,30 +1,31 @@
-export interface ProjectsInterface extends ObjectBase {
+export interface InactiveProjectsInterface extends ObjectBase {
   _view_: ProjectNameView;
-  Name: ObjectBase & { _view_: ProjectNameView };
+  Name: ObjectBase & {
+    _view_: ProjectNameView;
+  };
   Description: string;
   Projectleider: ObjectBase & {
     _view_: PersonNameView;
   };
   Status: string;
   Active: boolean;
-  _id_: string;
 }
 
-export interface PersonNameView {
+interface ObjectBase {
+  _id_: string;
+  _label_: string;
+  _path_: string;
+  _ifcs_: Array<InterfaceRefObject>;
+}
+
+interface PersonNameView {
   firstName: string;
   txt1: string;
   lastName: string;
 }
 
-export interface ProjectNameView {
+interface ProjectNameView {
   name: string;
-}
-
-export interface ObjectBase {
-  _id_: string;
-  _label_: string;
-  _path_: string;
-  _ifcs_: Array<InterfaceRefObject>;
 }
 
 interface InterfaceRefObject {
