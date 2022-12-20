6d1556c3101df0ac166641e12aabea525d89e31a
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-12-20 23:06:38 +0100

    Change Alphanumeric component input field to FormControl field
    Call patch() on value changes

--- src/app/shared/atomic-components/atomic-alphanumeric/atomic-alphanumeric.component.ts
+++ src/app/shared/atomic-components/atomic-alphanumeric/atomic-alphanumeric.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseAtomicComponent } from '../BaseAtomicComponent.class';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
+import { Resource } from '../../interfacing/resource.interface';
 
 @Component({
   selector: 'app-atomic-alphanumeric',
@@ -7,9 +10,34 @@ import { BaseAtomicComponent } from '../BaseAtomicComponent.class';
   styleUrls: ['./atomic-alphanumeric.component.css'],
 })
 export class AtomicAlphanumericComponent extends BaseAtomicComponent<string> {
-  newItem!: string;
+  value!: FormControl<string | null>;
+  newItem: FormControl<string> = new FormControl<string>('', {nonNullable: true});
+
+  @Input()
+  resource!: Resource<any>;
+
+  @Input()
+  propertyName!: string;
 
   isNewItemInputRequired() {
     return this.isTot && this.property?.length === 0;
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.value = new FormControl(this.requireArray(this.property)[0], {nonNullable: false});
+    this.value.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap(x => console.log(x)),
+      )
+      .subscribe(
+        x => this.resource.patch([{
+          op: 'replace',
+          path: this.propertyName,
+          value: x,
+        }]).subscribe()
+      );
   }
 }
