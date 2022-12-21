922674198c8dd74afd28c5ebf85725db14f7153b
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-12-21 22:27:30 +0100

    Implement patch functionality for ToolGalleryComponent

--- src/app/tools/tool-gallery/tool-gallery.component.ts
+++ src/app/tools/tool-gallery/tool-gallery.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { testdata } from '../../project-administration/active-projects/testdata';
+import { from, map, Observable, tap } from 'rxjs';
+import { PatchReplace } from 'src/app/shared/interfacing/patch-replace.interface';
+import { PatchResponse } from 'src/app/shared/interfacing/patch-response.interface';
+import { Resource } from 'src/app/shared/interfacing/resource.interface';
+import { TestDataInterface } from '../test-data.interface';
+import { testdata } from '../testdata';
+import { applyPatch } from 'fast-json-patch';
 
 @Component({
   selector: 'app-tool-gallery',
   templateUrl: './tool-gallery.component.html',
   styleUrls: ['./tool-gallery.component.scss'],
 })
-export class ToolGalleryComponent {
-  activeProjects = testdata[0];
+export class ToolGalleryComponent implements Resource<TestDataInterface> {
+  data = JSON.parse(JSON.stringify(testdata[0]));
 
   constructor() {}
+
+  patch(patches: PatchReplace[]): Observable<PatchResponse<TestDataInterface>> {
+    // Prepend path of patches with '/', because our backend has a different (probably wrong) implementation
+    // of the JSON-Patch (RFC6902) standard. The applyPatch function below requires the '/'.
+    patches.forEach((x) => (x.path = `/${x.path}`));
+
+    return from([
+      {
+        content: applyPatch(this.data, patches).newDocument,
+        patches: patches,
+        isCommitted: true,
+      } as PatchResponse<TestDataInterface>,
+    ]).pipe(
+      tap((x) => {
+        console.log(x);
+        if (x.isCommitted) {
+          this.data = x.content;
+        }
+      }),
+    );
+  }
 }
