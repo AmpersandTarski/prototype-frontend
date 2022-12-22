9e5543dde6a8030cfc2a89ba6be3a1a96d10d604
LiWeiYeh <liwei_yeh@hotmail.com>
2022-12-22 15:59:25 +0100

    removed unnecessary imports

--- src/app/project-administration/project-edit/project-edit.component.ts
+++ src/app/project-administration/project-edit/project-edit.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ɵɵsetComponentScope } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { from, Observable, reduce, switchMap, tap } from 'rxjs';
+import { from, Observable, switchMap, tap } from 'rxjs';
 import { PatchReplace } from 'src/app/shared/interfacing/patch-replace.interface';
 import { PatchResponse } from 'src/app/shared/interfacing/patch-response.interface';
 import { Resource } from 'src/app/shared/interfacing/resource.interface';
