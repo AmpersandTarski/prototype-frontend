9d3dc5fd2a35badb7d49224930b8f49b32efeb56
LiWeiYeh <liwei_yeh@hotmail.com>
2022-12-22 13:46:55 +0100

    placed properties in BaseAtomicComponent

--- src/app/shared/atomic-components/atomic-alphanumeric/atomic-alphanumeric.component.ts
+++ src/app/shared/atomic-components/atomic-alphanumeric/atomic-alphanumeric.component.ts
@@ -1,8 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseAtomicComponent } from '../BaseAtomicComponent.class';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs';
-import { Resource } from '../../interfacing/resource.interface';
 
 @Component({
   selector: 'app-atomic-alphanumeric',
@@ -10,29 +8,16 @@ import { Resource } from '../../interfacing/resource.interface';
   styleUrls: ['./atomic-alphanumeric.component.css'],
 })
 export class AtomicAlphanumericComponent extends BaseAtomicComponent<string> implements OnInit {
+  // make generic and put in BaseAtomicComponent class
   formControl!: FormControl<string | null>;
   newItemControl: FormControl<string> = new FormControl<string>('', { nonNullable: true, updateOn: 'blur' });
-
-  @Input()
-  // The type of the T for Resource<T> is not relevant nor to determine here; therefore unknow
-  // We require a Resource, that implements the required methods (like patch)
-  // Most likely this is a top-level component for a specific application interface (e.g. ProjectComponent)
-  resource!: Resource<unknown>; // TODO move to the base class. It is needed for all atomic components
-
-  @Input()
-  propertyName!: string; // TODO move to the base class. It is needed for all atomic components
 
   override ngOnInit(): void {
     super.ngOnInit();
     this.formControl = new FormControl(this.data[0], { nonNullable: false, updateOn: 'blur' });
 
-    this.formControl.valueChanges
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-        map((x) => (x === '' ? null : x)), // transform empty string to null value
-      )
-      .subscribe((x) =>
+    this.formControl.valueChanges.subscribe(
+      (x) =>
         this.resource
           .patch([
             {
@@ -42,6 +27,7 @@ export class AtomicAlphanumericComponent extends BaseAtomicComponent<string> imp
             },
           ])
           .subscribe(),
-      );
+      // .subscribe(() => this.resource.data$.subscribe((x: any) => console.log(x))),
+    );
   }
 }
