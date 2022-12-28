c32355705cf4fb567c67e6d69f7157446a92235f
Patrick Ramge <patrickramge@gmail.com>
2022-12-28 15:30:39 +0100

    modify interface to support different types of requests

--- src/app/shared/interfacing/patch-replace.interface.ts
+++ src/app/shared/interfacing/patch-replace.interface.ts
@@ -1,6 +1,9 @@
-export interface PatchReplace {
+export interface PatchGeneric {
   op: 'add' | 'remove' | 'replace';
   path: string;
   // make generic
+}
+
+export interface PatchReplace extends PatchGeneric {
   value: string | boolean | null;
 }
