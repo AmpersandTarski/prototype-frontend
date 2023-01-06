0f797e7ecf57110e7efeef6dbedb5b4685915e2f
Sheriff Balunywa <sheriffbalunywa@ba92-m3257whyr5.home>
2023-01-06 12:12:11 +0100

    feat: Added not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { AppLayoutComponent } from './layout/app.layout.component';
 import { HomeComponent } from './layout/home/home.component';
 import { ToolComponentDetailsComponent } from './tools/tool-component-details/tool-component-details.component';
 import { ToolGalleryComponent } from './tools/tool-gallery/tool-gallery.component';
+import { NotFoundComponentComponent } from './layout/not-found-component/not-found-component.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppLayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [{ path: '', component: HomeComponent },
+               { path: '404', component: NotFoundComponentComponent },
+               { path: '**', component: NotFoundComponentComponent },
+              ],
+  //]
   },
+  
   {
     path: 'tools',
     component: AppLayoutComponent,
--- src/app/project-administration/project/project.component.ts
+++ src/app/project-administration/project/project.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { BackendService } from '../backend.service';
 import { ProjectInterface } from './project.interface';
+import { Router, Navigation } from '@angular/router';
 
 @Component({
   selector: 'app-project',
@@ -13,14 +14,15 @@ export class ProjectComponent implements OnInit {
   public data$!: Observable<ProjectInterface>;
   public projectId?: string;
 
-  constructor(private route: ActivatedRoute, private service: BackendService) {}
+  constructor(private route: ActivatedRoute, private service: BackendService, private router: Router) {}
 
   ngOnInit(): void {
     this.data$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
         this.projectId = params.get('id')!;
         if (this.projectId === null) {
-          throw new Error('id does not exist');
+        //  throw new Error('id does not exist');
+          this.router.navigate(['/404'], { skipLocationChange: true });
         }
         return this.service.getProject(this.projectId);
       }),
