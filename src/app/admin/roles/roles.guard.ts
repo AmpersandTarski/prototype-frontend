30a738cb4b9026cf070f9e2bffa5f35ac5a41356
LiWeiYeh <liwei_yeh@hotmail.com>
2023-02-22 10:33:54 +0100

    feat: create guards for navigation based on the user's roles

---
+++ src/app/admin/roles/roles.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RolesService } from './roles.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RolesGuard implements CanActivate {
+  constructor(private rolesService: RolesService, private router: Router) {}
+
+  // can only activate if it has the required role
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    let isRole$: Observable<boolean> = this.rolesService.isRole(route.data['role']);
+    isRole$.subscribe((x) => {
+      if (!x) {
+        this.router.navigate(['']);
+      }
+    });
+    return isRole$;
+  }
+}
--- src/app/admin/roles/roles.service.ts
+++ src/app/admin/roles/roles.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Navbar, SessionRole } from 'src/app/shared/interfacing/navbar.interface';
+import { Role } from 'src/app/shared/models/roles';
 
 @Injectable()
 export class RolesService {
@@ -16,5 +17,9 @@ export class RolesService {
   public patchRole(roles: Array<SessionRole>, roleIndex: number): Observable<Array<SessionRole>> {
     roles[roleIndex].active = !roles[roleIndex].active; // reverses the boolean value
     return this.http.patch<Array<SessionRole>>('app/roles', roles);
+  }
+
+  public isRole(role: Role): Observable<boolean> {
+    return this.getRoles().pipe(map((x) => x[role].active));
   }
 }
