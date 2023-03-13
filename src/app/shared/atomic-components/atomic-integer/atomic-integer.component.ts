5e509e08068a922447663e6c6a1a04b866576651
Martijn van Andel <58219126+byTinus@users.noreply.github.com>
2023-03-13 12:05:24 +0100

    atomic float and int component added

--- src/app/shared/atomic-components/atomic-integer/atomic-integer.component.ts
+++ src/app/shared/atomic-components/atomic-integer/atomic-integer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BaseAtomicComponent } from '../BaseAtomicComponent.class';
 
 @Component({
@@ -6,4 +7,36 @@ import { BaseAtomicComponent } from '../BaseAtomicComponent.class';
   templateUrl: './atomic-integer.component.html',
   styleUrls: ['./atomic-integer.component.css'],
 })
-export class AtomicIntegerComponent<I> extends BaseAtomicComponent<number, I> {}
+export class AtomicIntegerComponent<I> extends BaseAtomicComponent<number, I> implements OnInit {
+  public formControl!: FormControl<number>;
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+
+    // univalent
+    if (this.isUni && this.canUpdate()) {
+      this.initFormControl();
+    }
+
+    // not univalent
+    if (!this.isUni && this.canUpdate()) {
+      this.newItemControl = new FormControl<number>(0, { nonNullable: true, updateOn: 'change' });
+    }
+  }
+
+  private initFormControl(): void {
+    this.formControl = new FormControl<number>(this.data[0], { nonNullable: true, updateOn: 'blur' });
+
+    this.formControl.valueChanges.subscribe((x) =>
+      this.interfaceComponent
+        .patch(this.resource, [
+          {
+            op: 'replace',
+            path: this.propertyName, // FIXME
+            value: x,
+          },
+        ])
+        .subscribe(),
+    );
+  }
+}
