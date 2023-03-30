30f227910403e02ce93f709b0aff77515e356e4e
Martijn van Andel <58219126+byTinus@users.noreply.github.com>
2023-03-30 09:11:28 +0200

    Removed test error

--- src/app/project-administration/active-projects/active-projects.component.ts
+++ src/app/project-administration/active-projects/active-projects.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { Observable } from 'rxjs';
 import { AmpersandInterface } from 'src/app/shared/interfacing/ampersand-interface.class';
 import { BackendService } from '../backend.service';
 import { ActiveProjectsInterface } from './active-projects.interface';
@@ -18,9 +17,5 @@ export class ActiveProjectsComponent extends AmpersandInterface<ActiveProjectsIn
 
   ngOnInit(): void {
     this.data$ = this.service.getActiveProjects();
-  }
-
-  ngAfterViewInit(): void {
-    throw new Error('Error in active projects: test!!');
   }
 }
