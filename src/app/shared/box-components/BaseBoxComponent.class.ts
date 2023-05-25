74fb87481af7cfa3ec6aee2a4efda7ca9c0ca949
LiWeiYeh <liwei_yeh@hotmail.com>
2023-05-25 13:54:08 +0200

    feat: Box-Components can add existing items

--- src/app/shared/box-components/BaseBoxComponent.class.ts
+++ src/app/shared/box-components/BaseBoxComponent.class.ts
@@ -1,19 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, of, tap } from 'rxjs';
 import { AmpersandInterface } from '../interfacing/ampersand-interface.class';
 import { ObjectBase } from '../objectBase.interface';
 
 @Component({
   template: '',
 })
-export abstract class BaseBoxComponent<TItem extends ObjectBase, I> {
+export abstract class BaseBoxComponent<TItem extends ObjectBase, I> implements OnInit {
   @Input() resource!: ObjectBase;
   @Input() propertyName!: string;
   @Input() data!: TItem[];
   @Input() interfaceComponent!: AmpersandInterface<I>;
   @Input() crud: string = 'cRud';
+  @Input() getPath!: string;
+  @Input() placeholder!: string;
+  dropdownMenuObjects$: Observable<ObjectBase[]> = of();
+  newItemControl!: FormControl<string | boolean | number | ObjectBase>;
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    if (this.canUpdate()) {
+      this.newItemControl = new FormControl<ObjectBase>({} as ObjectBase, { nonNullable: true, updateOn: 'change' });
+      this.dropdownMenuObjects$ = this.getDropdownMenuItems(this.getPath);
+    }
+  }
 
   public canCreate(): boolean {
     return this.crud[0] == 'C';
@@ -37,6 +50,38 @@ export abstract class BaseBoxComponent<TItem extends ObjectBase, I> {
     });
   }
 
+  public addItem() {
+    let val = this.newItemControl.value as ObjectBase;
+
+    this.interfaceComponent
+      .patch(this.resource._path_, [
+        {
+          op: 'add',
+          path: this.propertyName,
+          value: val._id_,
+        },
+      ])
+      .subscribe((x) => {
+        if (x.isCommitted && x.invariantRulesHold) {
+          // TODO: fix ugly any type
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          this.data = (x.content as any)[this.propertyName] as TItem[];
+
+          // remove the recently added item from the dropdown menu
+          this.dropdownMenuObjects$ = this.dropdownMenuObjects$.pipe(
+            tap((objects) =>
+              objects.forEach((item, index) => {
+                if (item._id_ === val._id_) {
+                  objects.splice(index, 1);
+                }
+              }),
+            ),
+          );
+          this.newItemControl.setValue({} as ObjectBase);
+        }
+      });
+  }
+
   public removeItem(index: number): void {
     this.interfaceComponent
       .patch(this.resource._path_, [
@@ -48,6 +93,7 @@ export abstract class BaseBoxComponent<TItem extends ObjectBase, I> {
       .subscribe((x) => {
         if (x.isCommitted && x.invariantRulesHold) {
           this.data.splice(index, 1);
+          this.dropdownMenuObjects$ = this.dropdownMenuObjects$.pipe(tap((objects) => objects.push(this.data[index])));
         }
       });
   }
@@ -58,5 +104,13 @@ export abstract class BaseBoxComponent<TItem extends ObjectBase, I> {
         this.data.splice(index, 1);
       }
     });
+  }
+
+  private getDropdownMenuItems(path: string): Observable<ObjectBase[]> {
+    let objects: Observable<ObjectBase[]> = this.interfaceComponent.get(path);
+    objects = objects.pipe(
+      map((dropdownobjects) => dropdownobjects.filter((object) => !this.data.map((y) => y._id_).includes(object._id_))),
+    );
+    return objects;
   }
 }
