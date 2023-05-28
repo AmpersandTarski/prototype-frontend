11e8312c1be7abce002489baf2015dcefecdf1af
hanjoosten <han.joosten@ordina.nl>
2023-05-28 20:37:19 +0200

    First attempt to get the documentation online

---
+++ docs/sidebar.js
@@ -0,0 +1,19 @@
+module.exports = {
+    prototypeFrontendLandingpagesSidebar: [
+    ],
+    prototypeFrontendGuideSidebar: [
+        // This is for all documentation from the prototype-frontend repo that should go in the `Guides` part of the menu.
+    ],
+    prototypeFrontendReferenceSidebar: [
+        // This is for all documentation from the prototype-frontend repo that should go in the `Reference material` part of the menu.
+        {
+            type: 'category',
+            label: 'Prototype frontend',
+            items: [
+                'prototype-frontend/reference-material/the-prototype-frontend',
+                ]
+        },
+    ],
+    prototypeFrontendTheorySidebar: [
+    ],
+};
