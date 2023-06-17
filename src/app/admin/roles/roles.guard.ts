4cb04395a950a4cd92117caec750ca92044ec607
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-06-17 14:25:48 +0200

    Update Angular and primeng to v16

--- src/app/admin/roles/roles.guard.ts
+++ src/app/admin/roles/roles.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { RolesService } from './roles.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RolesGuard implements CanActivate {
+export class RolesGuard  {
   constructor(private rolesService: RolesService, private router: Router) {}
 
   // can only activate if it has the required role
